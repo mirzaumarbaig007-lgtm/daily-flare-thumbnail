be6c5f23f8e7a173d16e83f335e3446d6cd42374
mirzaumarbaig007-lgtm <mirzaumarbaig007@gmail.com>
2026-09-19 01:31:14 +0200

    Fix FlowRow build imports and experimental API

--- app/src/main/java/com/thedailyflare/thumbnail/MainActivity.kt
+++ app/src/main/java/com/thedailyflare/thumbnail/MainActivity.kt
@@ -37,6 +37,7 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
@@ -464,6 +465,7 @@ class MainActivity : ComponentActivity() {
     }
 
 
+    @OptIn(androidx.compose.foundation.layout.ExperimentalLayoutApi::class)
     @Composable
     private fun TextHighlightDialog(
         initialText: String,
