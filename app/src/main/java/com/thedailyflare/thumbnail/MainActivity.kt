9e117f97b7a2afa69a184026c26bbf4a51a7e587
mirzaumarbaig007-lgtm <mirzaumarbaig007@gmail.com>
2026-09-19 23:24:02 +0200

    Fix headline overlay build imports

--- app/src/main/java/com/thedailyflare/thumbnail/MainActivity.kt
+++ app/src/main/java/com/thedailyflare/thumbnail/MainActivity.kt
@@ -72,6 +72,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.graphics.Brush
